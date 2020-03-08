ad1e866d8352ddbb4aa09d61ec199e75b09b20bf
DanielCriollo <danielcriollo9706@gmail.com>
2020-03-08 01:29:10 -0500

    Nuevos ejercicios

--- Estructuras de Datos/Vectores/App1_Ejemplo/src/app1_ejemplo/ventana.java
+++ Estructuras de Datos/Vectores/App1_Ejemplo/src/app1_ejemplo/ventana.java
@@ -118,7 +118,7 @@ public class ventana extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ventana().setVisible(true);
+               
             }
         });
     }
